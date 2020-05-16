e572aca602580a052c78d245f3eaf9b04098b76a
watterfall <watterfall@163.com>
2020-05-16 14:20:50 +0800

    change deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 set -e
 
-sudo npm run build
+npm run build
 cd public
 
 echo 'openfiesta.space' > CNAME
 
-sudo git init
-sudo git add -A
-sudo git commit -m 'deploy2'
+git init
+git add -A
+git commit -m 'deploy2'
 
-sudo git push -f git@github.com:openfiesta/openfiesta.space.git master
+git push -f git@github.com:openfiesta/openfiesta.space.git master
 
 cd -
